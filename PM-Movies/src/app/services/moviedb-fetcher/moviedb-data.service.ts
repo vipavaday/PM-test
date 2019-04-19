dbbc0719f0d10195c6e168e638992880976df515
Pavaday <victor.pavaday@perfect-memory.com>
2019-04-19 14:35:16 +0200

    feat(code): Added specs for moviedb-data-fetcher service

--- PM-Movies/src/app/services/moviedb-fetcher/moviedb-data.service.ts
+++ PM-Movies/src/app/services/moviedb-fetcher/moviedb-data.service.ts
@@ -10,21 +10,22 @@ import {
 import {
   map,
   withLatestFrom,
-  shareReplay
+  shareReplay,
+  tap
 } from 'rxjs/operators';
 
 import {
   Content,
   Config,
-  Person
+  Person,
+  ContentType
 } from '../../models';
 
 import {
   MDBSearchResponseJSON,
-  MDBMovieJSON,
-  MDBTvShowJSON,
   ContentParserService,
-  MDBContentImagesJSON
+  MDBContentImagesJSON,
+  MDBContentJSON
 } from '../content-parser';
 
 import {
@@ -63,39 +64,27 @@ export class MoviedbDataService {
      **/
   public getContentInfo(title: string): Observable<Content[]> {
 
-    if (title === '') {
+    if (title === undefined || title === '') {
       return of([]);
     }
 
     const search$ = this.http.get<MDBSearchResponseJSON>(`${this.baseUrl}/search/multi?api_key=${this.apiKey}&query=${encodeURI(title)}`);
 
-    return search$.pipe(
-      withLatestFrom(this.imgBaseUrl$),
-      map(([response, imgBaseUrl]) => this.parseContentList(response, imgBaseUrl).filter(content => !!content))
-    );
+    return this.extractContentList(search$);
   }
 
   /**
   * Retrieves some information about a TV Show or Movie
   **/
-  public getContentDetails(type: string, id: number): Observable<Content> {
+  public getContentDetails(type: ContentType, id: number): Observable<Content> {
 
+    if (!type || !id) {
+      throw new Error('#getContentDetails: content type and id should not be undefined');
+    }
+    const details$ = this.http.get<MDBContentJSON>(`${this.baseUrl}/${type}/${id}?api_key=${this.apiKey}`);
     const credits$ = this.http.get<MDBCreditsJSON>(`${this.baseUrl}/${type}/${id}/credits?api_key=${this.apiKey}`);
-    const details$ = this.http.get(`${this.baseUrl}/${type}/${id}?api_key=${this.apiKey}`);
 
-    return zip(details$, credits$).pipe(
-      withLatestFrom(this.imgBaseUrl$),
-      map(([[contentDetail, credits], imgBaseUrl]: [[MDBMovieJSON | MDBTvShowJSON, MDBCreditsJSON], string]) => {
-        let content: Content;
-        if (type === 'movie') {
-          content = this.contentParser.parseMovie((<MDBMovieJSON>contentDetail), credits, imgBaseUrl);
-        } else if (type === 'tv') {
-          content = this.contentParser.parseTvShow((<MDBTvShowJSON>contentDetail), credits, imgBaseUrl);
-        }
-        content.cast = this.peopleParser.parseCredits(credits);
-
-        return content;
-      }));
+    return this.extractContentDetails(details$, credits$, type);
   }
 
   /**
@@ -104,21 +93,46 @@ export class MoviedbDataService {
    */
   public getPersonDetails(personId: number, person?: Person): Observable<Person> {
 
+    if (!personId) {
+      throw new Error('#getPersonDetails: personId should not be undefined');
+    }
+
     return this.http.get<MDBPersonJSON>(`${this.baseUrl}/person/${personId}?api_key=${this.apiKey}`)
       .pipe(withLatestFrom(this.imgBaseUrl$),
         map(([personJson, imgBaseUrl]) => this.peopleParser.parsePerson(personJson, imgBaseUrl, person)));
   }
 
   public getExtraImages(content: Content): Observable<string[]> {
+    if (!content) {
+      throw new Error('#getExtraImages : content should not be undefined');
+    }
 
     return this.http.get<MDBContentImagesJSON>(`${this.baseUrl}/${content.type}/${content.tmdbId}/images?api_key=${this.apiKey}`)
-      .pipe(withLatestFrom(this.imgBaseUrl$),
-        map(([contentImgJson, imgBaseUrl]) => this.contentParser.parseContentImages(contentImgJson, imgBaseUrl, content.backdrops))
+      .pipe(
+        withLatestFrom(this.imgBaseUrl$),
+        map(([contentImgJson, imgBaseUrl]) => this.contentParser.parseContentImages(contentImgJson, imgBaseUrl, content))
       );
   }
 
-  private parseContentList(response: MDBSearchResponseJSON, imgBaseUrl: string) {
-    return response.results.map(res => this.contentParser.parse(res, imgBaseUrl));
+  private extractContentList(search$: Observable<MDBSearchResponseJSON>): Observable<Content[]> {
+    return search$.pipe(
+      withLatestFrom(this.imgBaseUrl$),
+      map(([response, imgBaseUrl]) => {
+        return this.contentParser.parseContentList(response, imgBaseUrl).filter(content => !!content);
+      }));
   }
 
+  private extractContentDetails(
+    details$: Observable<MDBContentJSON>,
+    credits$: Observable<MDBCreditsJSON>,
+    type: ContentType
+  ): Observable<Content> {
+
+    return zip(details$, credits$).pipe(withLatestFrom(this.imgBaseUrl$), map(([[contentDetail, credits], imgBaseUrl]) => {
+      contentDetail.media_type = type;
+      const content = this.contentParser.parse(contentDetail, imgBaseUrl);
+      this.peopleParser.parseContentCredits(credits, content);
+      return content;
+    }));
+  }
 }
