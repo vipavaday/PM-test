16acdad9c651d3d00f9511b504e1c3ceeabc04b4
Pavaday <victor.pavaday@perfect-memory.com>
2019-04-25 11:36:07 +0200

    fix(code): moved release date validation into filter model
    release date validation previously were in filter panel component

--- PM-Movies/src/app/models/filter/filter.spec.ts
+++ PM-Movies/src/app/models/filter/filter.spec.ts
@@ -1,6 +1,12 @@
 import { Filter } from './filter';
 
 describe('Models: Filter', () => {
+  let filter: Filter;
+
+  beforeEach(() => {
+    filter = new Filter();
+  });
+
   describe('#new', () => {
     it('should create an instance', () => {
       expect(() => new Filter()).not.toThrow();
@@ -20,13 +26,6 @@ describe('Models: Filter', () => {
   });
 
   describe('#toggleContentType', () => {
-
-    let filter: Filter;
-
-    beforeEach(() => {
-      filter = new Filter();
-    });
-
     it('should add content type when missing', () => {
       filter.contentTypes.clear();
       filter.toggleContentType('tv');
@@ -43,6 +42,30 @@ describe('Models: Filter', () => {
       filter.toggleContentType('movie');
       expect(filter.contentTypes.has('tv')).toEqual(true);
       expect(filter.contentTypes.has('movie')).toEqual(false);
+    });
+  });
+
+  describe('#set ltReleaseDate', () => {
+    it('should throw an error when parameter is not parsable as a date', () => {
+      expect(() => filter.ltReleaseDate = '82/24/1987')
+      .toThrowError('#set ltReleaseDate: ltDate should follow dd/mm/yyyy formmat');
+    });
+
+    it('should set filter lower than date property', () => {
+      filter.ltReleaseDate = '12/12/1987';
+      expect(filter.ltReleaseDate).toBe('12/12/1987');
+    });
+  });
+
+  describe('#set gtReleaseDate', () => {
+    it('should throw an error when parameter is not parsable as a date', () => {
+      expect(() => filter.gtReleaseDate = '82/24/1987')
+      .toThrowError('#set gtReleaseDate: gtDate should follow dd/mm/yyyy formmat');
+    });
+
+    it('should set filter greater than date property', () => {
+      filter.gtReleaseDate = '12/12/1987';
+      expect(filter.gtReleaseDate).toBe('12/12/1987');
     });
   });
 });
