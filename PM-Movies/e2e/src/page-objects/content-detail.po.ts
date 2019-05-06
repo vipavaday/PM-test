5688d59363c5de728d4cb9fef417c5dca6832c1c
Victor Pavaday <victor.pavaday@perfect-memory.com>
2019-05-06 12:19:08 +0200

    fix(code): removed explicit port usage in page object for e2e specs

--- PM-Movies/e2e/src/page-objects/content-detail.po.ts
+++ PM-Movies/e2e/src/page-objects/content-detail.po.ts
@@ -8,6 +8,6 @@ export class ContentDetailPage {
   public rootEl = $('.detail-container');
 
   public navigateTo(contentType: ContentType, contentId: string) {
-    browser.get(`http://localhost:4200/detail/${contentType}/${contentId}`);
+    browser.get(`/detail/${contentType}/${contentId}`);
   }
 }
